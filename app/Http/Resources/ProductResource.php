47ff62a4448cf008e6401ce0173be1b2c5617bc6
mestashh <zeuszeus1973@gmail.com>
2026-04-20 20:41:57 +0300

    Create category and product resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $id
+ * @property mixed $name
+ * @property mixed $description
+ * @property mixed $price
+ * @property mixed $category_id
+ */
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'category_id' => $this->category_id,
+        ];
+    }
+}
